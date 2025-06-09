f3a3036738bc382927d02ca7b3e4665598cfa793
YannieChiem <s0409425@salemstate.edu>
2025-06-09 06:01:26 -0400

    Initial Commit

---
+++ ZeroWaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZeroWaste
+//
+//  Created by Yannie Chiem on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZeroWaste/ZeroWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroWasteApp.swift
+//  ZeroWaste
+//
+//  Created by Yannie Chiem on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ZeroWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
